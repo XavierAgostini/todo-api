bdd4468c2252eea6a433db173691cc45b7ff2424
XavierAgostini <xavier.agostini@mail.mcgill.ca>
2018-09-01 11:49:21 -0700

    add jwt support

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+var env = process.env.NODE_ENV || 'development'
+console.log('env ******', env)
+if (env === 'development') {
+  process.env.PORT = 3000
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+} else if (env === 'test') {
+  process.env.PORT = 3000
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+}
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
@@ -8,7 +17,6 @@ var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
 
 var app = express()
-const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
@@ -86,8 +94,45 @@ app.patch('/todos/:id', (req, res) => {
   })
 })
 
-app.listen(port, () => {
-  console.log(`started server on port ${port}\n`)
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
+
+app.get('/users/:id', (req, res) => {
+  console.log('get users')
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  User.findById(id).then((user) => {
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send({user})
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
+
+app.delete('/users', (req, res) => {
+  User.remove({}).then((user) => {
+    res.send(user)
+  }, (e) => {
+    res.status(404).send(e)
+  })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`started server on port ${process.env.PORT}\n`)
 })
 
 module.exports = {
