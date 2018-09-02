463f1debc64f5f2dc5e811d4306ca01adb18c4e7
XavierAgostini <xavier.agostini@mail.mcgill.ca>
2018-09-01 21:37:45 -0700

    add tests for delete /users/me/tokens

--- server/server.js
+++ server/server.js
@@ -129,7 +129,7 @@ app.post('/users/login', (req, res) => {
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
     res.sendStatus(200)
-  }, () => {
+  }, (e) => {
     res.sendStatus(400)
   })
 })
