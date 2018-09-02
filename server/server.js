f6b1969ff147552e58f42809bc3b48ad16a343e7
XavierAgostini <xavier.agostini@mail.mcgill.ca>
2018-09-01 20:46:15 -0700

    add post /users/login route'

--- server/server.js
+++ server/server.js
@@ -114,6 +114,18 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
 })
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user)
+    })
+  }).catch((e) => {
+    res.sendStatus(400)
+  })
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`started server on port ${process.env.PORT}\n`)
 })
